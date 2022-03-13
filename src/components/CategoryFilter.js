6a7995a97784548821d5d1a5be085909f8a294ec
Joe <hutcheson2001@yahoo.com>
2022-03-13 12:37:33 -0400

    Submitting assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, catFilter}) {
+  const buttonList = categories.map((category)=>{
+    return (
+      <button key={category} onClick={catFilter} id={category}>{category}</button>
+    )
+  })
+
   return (
-    <div className="categories">
+    <div className={categories}>
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {buttonList}
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Task from "./Task"
+// import { v4 as uuid } from "uuid";
 
-function TaskList() {
+function TaskList({tasks, setTaskList}) {
+  const thisTaskList = tasks.map(task => 
+      <Task text={task.text} key={task.text} category={task.category} deleteTask={deleteTask}/>
+  )
+
+  function deleteTask(e){
+    const thisTask = e.target.name
+    const newTaskList = tasks.filter((task) => (task.text !== thisTask))
+    setTaskList(newTaskList)
+  }
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {thisTaskList}
     </div>
   );
 }
